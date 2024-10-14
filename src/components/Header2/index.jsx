ba28d969119d1ceab86aa8e150f71802daabb0f1
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-10-14 21:13:24 +0700

    Update header search

--- src/components/Header2/index.jsx
+++ src/components/Header2/index.jsx
@@ -109,7 +109,20 @@ export default function Header2({ ...props }) {
                           </div>
                       </div>
                       <Button size="md" className="mt rounded-lg sm:mt-0">
-                          Search
+                          <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="h-4 w-4"
+                          >
+                              <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                              />
+                          </svg>
                       </Button>
                   </div>
 
