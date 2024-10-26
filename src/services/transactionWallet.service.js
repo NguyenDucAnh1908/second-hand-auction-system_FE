38e9c44074e9ab78c16302a1d3367016cec78a42
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-10-26 16:41:42 +0700

    Call api update user anh api main-category

---
+++ src/services/transactionWallet.service.js
@@ -0,0 +1,24 @@
+import {apiSlice} from "../redux/api/apiSlice.js";
+
+export const transactionWalletApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getTransactionWallet: builder.query({
+            query: (paging) => ({
+                url: "transactionWallet/get-transaction",
+                params: {
+                    page: paging.page || 0,
+                    limit: paging.limit || 10,
+                },
+            }),
+            transformResponse: (response) => {
+                return {
+                    items: response.data.data || [],
+                    totalPages: response.data.totalPages || 0,
+                    totalProducts: response.data.totalElements || 0,
+                };
+            },
+        }),
+    }),
+});
+
+export const {useGetTransactionWalletQuery} = transactionWalletApiSlice;
