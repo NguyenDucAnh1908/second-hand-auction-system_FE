6babe15f5e9ac2e435a4d91d85a7019b5afed50a
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-10-31 16:34:41 +0700

    View info in header seller

--- src/components/DashboardSeller/DropdownProfile.jsx
+++ src/components/DashboardSeller/DropdownProfile.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import Transition from '../../utils/Transition';
-
 import UserAvatar from '../../images/user-avatar-32.png';
-
+import {logOut} from "@/redux/auth/authSlice.js";
+import {useDispatch} from "react-redux";
 function DropdownProfile({
-  align
+  align, data
 }) {
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const trigger = useRef(null);
   const dropdown = useRef(null);
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   // close on click outside
   useEffect(() => {
     const clickHandler = ({ target }) => {
@@ -33,6 +34,10 @@ function DropdownProfile({
     document.addEventListener('keydown', keyHandler);
     return () => document.removeEventListener('keydown', keyHandler);
   });
+  const handleLogout = () => {
+    dispatch(logOut());
+    navigate("/login");
+  };
 
   return (
     <div className="relative inline-flex">
@@ -43,9 +48,9 @@ function DropdownProfile({
         onClick={() => setDropdownOpen(!dropdownOpen)}
         aria-expanded={dropdownOpen}
       >
-        <img className="w-8 h-8 rounded-full" src={UserAvatar} width="32" height="32" alt="User" />
+        <img className="w-8 h-8 rounded-full" src={data?.avatar} width="32" height="32" alt="User" />
         <div className="flex items-center truncate">
-          <span className="truncate ml-2 text-sm font-medium text-gray-600 dark:text-gray-100 group-hover:text-gray-800 dark:group-hover:text-white">Acme Inc.</span>
+          <span className="truncate ml-2 text-sm font-medium text-gray-600 dark:text-gray-100 group-hover:text-gray-800 dark:group-hover:text-white">{data?.fullName}</span>
           <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-500" viewBox="0 0 12 12">
             <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
           </svg>
@@ -68,8 +73,8 @@ function DropdownProfile({
           onBlur={() => setDropdownOpen(false)}
         >
           <div className="pt-0.5 pb-2 px-3 mb-1 border-b border-gray-200 dark:border-gray-700/60">
-            <div className="font-medium text-gray-800 dark:text-gray-100">Acme Inc.</div>
-            <div className="text-xs text-gray-500 dark:text-gray-400 italic">Administrator</div>
+            <div className="font-medium text-gray-800 dark:text-gray-100">{data?.fullName}</div>
+            <div className="text-xs text-gray-500 dark:text-gray-400 italic">{data?.role}</div>
           </div>
           <ul>
             <li>
@@ -84,8 +89,9 @@ function DropdownProfile({
             <li>
               <Link
                 className="font-medium text-sm text-violet-500 hover:text-violet-600 dark:hover:text-violet-400 flex items-center py-1 px-3"
-                to="/signin"
-                onClick={() => setDropdownOpen(!dropdownOpen)}
+                //to="/signin"
+                // onClick={() => setDropdownOpen(!dropdownOpen)}
+                onClick={handleLogout}
               >
                 Sign Out
               </Link>
--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -5,6 +5,8 @@ import Notifications from '../components/DashboardSeller/DropdownNotifications';
 import Help from '../components/DashboardSeller/DropdownHelp';
 import UserMenu from '../components/DashboardSeller/DropdownProfile';
 import ThemeToggle from '../components/DashboardSeller/ThemeToggle';
+import {useGetUserByIdQuery} from "@/services/user.service.js";
+
 
 function Header({
   sidebarOpen,
@@ -13,7 +15,7 @@ function Header({
 }) {
 
   const [searchModalOpen, setSearchModalOpen] = useState(false)
-
+  const { data: userHeader, error, isLoading: isUserLoading, refetch  } = useGetUserByIdQuery();
   return (
     <header className={`sticky top-0 before:absolute before:inset-0 before:backdrop-blur-md max-lg:before:bg-white/90 dark:max-lg:before:bg-gray-800/90 before:-z-10 z-30 ${variant === 'v2' || variant === 'v3' ? 'before:bg-white after:absolute after:h-px after:inset-x-0 after:top-full after:bg-gray-200 dark:after:bg-gray-700/60 after:-z-10' : 'max-lg:shadow-sm lg:before:bg-gray-100/90 dark:lg:before:bg-gray-900/90'} ${variant === 'v2' ? 'dark:before:bg-gray-800' : ''} ${variant === 'v3' ? 'dark:before:bg-gray-900' : ''}`}>
       <div className="px-4 sm:px-6 lg:px-8">
@@ -66,7 +68,7 @@ function Header({
             <ThemeToggle />
             {/*  Divider */}
             <hr className="w-px h-6 bg-gray-200 dark:bg-gray-700/60 border-none" />
-            <UserMenu align="right" />
+            <UserMenu align="right" data={userHeader} />
 
           </div>
 
