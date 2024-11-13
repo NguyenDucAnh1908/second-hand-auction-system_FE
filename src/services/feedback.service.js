9269ba3a558474ef77f97780b1d5eb8e3b654471
chanhtong123 <chanhtong11@gmail.com>
2024-11-14 06:42:43 +0700

    feedback buyer when order "CONFIRMED"

--- src/services/feedback.service.js
+++ src/services/feedback.service.js
@@ -1,12 +1,30 @@
 import { apiSlice } from "../redux/api/apiSlice.js";
 
 export const feedbackApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-      getFeedbackBySellerUserId: builder.query({
-        query: ({ userId, page = 0, size = 10 }) => `/feedback/seller/${userId}?page=${page}&size=${size}`, 
+  endpoints: (builder) => ({
+    getFeedbackBySellerUserId: builder.query({
+      query: ({ userId, page = 0, size = 10 }) => `/feedback/seller/${userId}?page=${page}&size=${size}`,
+    }),
+
+
+
+    createFeedback: builder.mutation({
+      query: (feedbackDto) => ({
+        url: '/feedback',
+        method: 'POST',
+        body: feedbackDto,
       }),
     }),
-  });
-  
 
-export const { useGetFeedbackBySellerUserIdQuery } = feedbackApiSlice;
+
+
+  }),
+});
+
+
+
+export const
+  {
+    useGetFeedbackBySellerUserIdQuery,
+    useCreateFeedbackMutation
+  } = feedbackApiSlice;
