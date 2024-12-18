651bbcaa131bbc519715e6ccbcedecd94de58575
Anhquan <anhquanpro332002@gmail.com>
2024-12-19 02:30:18 +0700

    bo sung lat vat

--- src/pages/Admin/ManageProduct/index.jsx
+++ src/pages/Admin/ManageProduct/index.jsx
@@ -1,26 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import {useGetItemAdminQuery} from '../../../services/item.service';
-import {Table, Image, Spin, Alert, Tag, Modal, Skeleton} from 'antd';
+import  {useState, useEffect} from 'react';
+import {useGetItemAdminQuery} from '@/services/item.service.js';
+import {Table, Image, Alert, Tag, Modal, Skeleton} from 'antd';
 import Pagination from "@/components/Pagination/index.jsx";
 import {useNavigate} from 'react-router-dom';
 import DescriptionItem from "@/components/DescriptionItem/index.jsx";
-import {Button, Typography} from "@material-tailwind/react";
+import {Button} from "@material-tailwind/react";
 
 export default function ProductPending() {
     const [page, setPage] = useState(1);
     const [isModalDescriptionVisible, setIsModalDescriptionVisible] = useState(false);
     const [selectedDescription, setSelectedDescription] = useState(null);
     const navigate = useNavigate('');
-    // Fetching data with currentPage and pageSize
     const {data, error, isLoading, refetch} = useGetItemAdminQuery({page: page - 1, limit: 10});
-    // Re-fetch data whenever currentPage changes
     useEffect(() => {
         refetch();
     }, [refetch]);
 
-    // if (isLoading) {
-    //     return <Spin size="large"/>;
-    // }
+
 
     if (error) {
         return <Alert message="Error loading products." type="error"/>;
@@ -42,19 +38,19 @@ export default function ProductPending() {
     };
     const columns = [
         {
-            title: 'ID',
-            dataIndex: 'itemId',
-            key: 'itemId',
+            title: "ID",
+            dataIndex: "itemId",
+            key: "itemId",
         },
         {
-            title: 'Tên sản phẩm',
-            dataIndex: 'itemName',
-            key: 'itemName',
+            title: "Tên sản phẩm",
+            dataIndex: "itemName",
+            key: "itemName",
         },
         {
-            title: 'Mô tả',
-            dataIndex: 'itemDescription',
-            key: 'itemDescription',
+            title: "Mô tả",
+            dataIndex: "itemDescription",
+            key: "itemDescription",
             render: (itemDescription, record) => (
                 <Button
                     onClick={() => handleOpenDescriptionModal(record.itemDescription)}
@@ -65,76 +61,72 @@ export default function ProductPending() {
             ),
         },
         {
-            title: 'Giá mua ngay',
-            dataIndex: 'priceBuyNow',
-            key: 'priceBuyNow',
-            render: (priceBuyNow) => {
-                return new Intl.NumberFormat('vi-VN', {
-                    style: 'currency',
-                    currency: 'VND',
-                }).format(priceBuyNow);
-            },
+            title: "Giá mua ngay",
+            dataIndex: "priceBuyNow",
+            key: "priceBuyNow",
+            render: (priceBuyNow) =>
+                new Intl.NumberFormat("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                }).format(priceBuyNow),
         },
-
         {
-            title: 'Trang thái',
-            dataIndex: 'itemStatus',
-            key: 'itemStatus',
+            title: "Trạng thái",
+            dataIndex: "itemStatus",
+            key: "itemStatus",
             render: (status) => {
                 let color;
                 switch (status) {
-                    case 'PENDING':
-                        color = 'orange';
+                    case "PENDING":
+                        color = "orange";
                         break;
-                    case 'APPROVED':
-                        color = 'green';
+                    case "APPROVED":
+                        color = "green";
                         break;
-                    case 'REJECTED':
-                        color = 'red';
+                    case "REJECTED":
+                        color = "red";
                         break;
                     default:
-                        color = 'gray';
+                        color = "gray";
                 }
                 return <Tag color={color}>{status}</Tag>;
             },
         },
         {
-            title: 'Hình ảnh',
-            dataIndex: 'thumbnail',
-            key: 'thumbnail',
-            render: (thumbnail, record) => (
+            title: "Hình ảnh",
+            dataIndex: "thumbnail",
+            key: "thumbnail",
+            render: (thumbnail, record) =>
                 thumbnail ? (
-                    <Image width={100} src={thumbnail} alt={record.itemName}/>
+                    <Image width={100} src={thumbnail} alt={record.itemName} />
                 ) : (
                     <span>No Image</span>
-                )
-            ),
+                ),
         },
         {
-            title: 'Ngày đăng kí',
-            dataIndex: 'create_at',
-            key: 'createAt',
-            render: (text) => text ? new Date(text).toLocaleDateString('vi-VN') : 'N/A',
+            title: "Ngày đăng kí",
+            dataIndex: "create_at",
+            key: "createAt",
+            render: (text) => (text ? new Date(text).toLocaleDateString("vi-VN") : "N/A"),
         },
         {
-            title: 'Người đăng kí',
-            dataIndex: 'createBy',
-            key: 'createBy',
+            title: "Người đăng kí",
+            dataIndex: "createBy",
+            key: "createBy",
         },
         {
-            title: 'Hành động',
-            key: 'action',
+            title: "Hành động",
+            key: "action",
             render: (_, record) => (
-                <>
-                    <Button onClick={() => handleAppraisal(record.itemId)}
-                            className="bg-green-500 text-white hover:bg-green-700">Thẩm định
-                    </Button>
-
-                </>
+                <Button
+                    onClick={() => handleAppraisal(record.itemId)}
+                    className="bg-green-500 text-white hover:bg-green-700 px-4 py-2 rounded-lg"
+                >
+                    Thẩm định
+                </Button>
             ),
         },
     ];
-
     return (
         <>
             <Modal
@@ -142,26 +134,31 @@ export default function ProductPending() {
                 visible={isModalDescriptionVisible}
                 onCancel={handleCloseDescriptionModal}
                 footer={[
-                    <Button key="close" onClick={handleCloseDescriptionModal}
-                            className="bg-red-500 text-white hover:bg-red-600">
+                    <Button
+                        key="close"
+                        onClick={handleCloseDescriptionModal}
+                        className="bg-red-500 text-white hover:bg-red-600"
+                    >
                         Đóng
                     </Button>,
                 ]}
                 className="rounded-lg shadow-lg"
                 width={1000}
             >
-                <DescriptionItem selectedDescription={selectedDescription}/>
+                <DescriptionItem selectedDescription={selectedDescription} />
             </Modal>
 
-            <div className="container mx-auto p-4">
-                <h1 className="text-2xl font-bold mb-4">Product Pending</h1>
-                <div className="flex items-center justify-center w-full h-full">
+            <div className="container mx-auto px-0">
+                <div className="flex justify-center">
                     {isLoading ? (
-                        <Skeleton active/>
+                        <div className="w-full">
+                            <Skeleton active title={false} paragraph={{ rows: 6 }} />
+                        </div>
                     ) : (
                         <Table
                             dataSource={pendingProducts}
                             columns={columns}
+                            className="w-full table-fixed text-sm border border-gray-300"
                             rowKey="itemId"
                             pagination={false}
                         />
@@ -169,12 +166,14 @@ export default function ProductPending() {
                 </div>
                 <div className="flex justify-center items-center mt-4">
                     <Pagination
-                        currentPage={page}
-                        totalPages={data?.data.totalPages || 1}
-                        onPageChange={setPage}
+                        current={page}
+                        total={(data?.data.totalPages || 1) * 10} // Tổng số mục (not tổng số trang)
+                        onChange={setPage}
+                        pageSize={10} // Số lượng mục trên mỗi trang
                     />
                 </div>
             </div>
         </>
     );
+
 }
